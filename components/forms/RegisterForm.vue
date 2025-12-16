5d720928f8373c7da96d4e47a7864ff1cc966d83
gary <yellow90249@gmail.com>
2025-12-16 14:05:38 +0800

    feat: 註冊頁面

--- components/forms/RegisterForm.vue
+++ components/forms/RegisterForm.vue
@@ -1,50 +1,43 @@
 <template>
- <Form :validation-schema="schema" @submit="onSubmit">
-      <div class="mb-20">
-        <label for="name">Username <span>**</span></label>
-        <Field name="name" id="name" type="text" placeholder="Enter Username" />
-        <ErrorMessage name="name" class="text-danger" />
-      </div>
+  <Form :validation-schema="schema" @submit="onSubmit">
+    <div class="mb-20">
+      <label for="name">姓名 <span>**</span></label>
+      <Field name="name" id="name" type="text" placeholder="輸入姓名..." />
+      <ErrorMessage name="name" class="text-danger" />
+    </div>
 
-      <div class="mb-20">
-        <label for="email-id">Email Address <span>**</span></label>
-        <Field name="email" id="email-id" type="text" placeholder="Email address..." />
-        <ErrorMessage name="email" class="text-danger" />
-      </div>
+    <div class="mb-20">
+      <label for="email-id">信箱 <span>**</span></label>
+      <Field name="email" id="email-id" type="text" placeholder="輸入信箱..." />
+      <ErrorMessage name="email" class="text-danger" />
+    </div>
 
-      <div class="mb-20">
-        <label for="pass">Password <span>**</span></label>
-        <Field name="password" id="pass" type="password" placeholder="Enter password..." />
-        <ErrorMessage name="password" class="text-danger" />
-      </div>
+    <div class="mb-20">
+      <label for="pass">密碼 <span>**</span></label>
+      <Field name="password" id="pass" type="password" placeholder="輸入密碼..." />
+      <ErrorMessage name="password" class="text-danger" />
+    </div>
 
-      <div class="mt-10"></div>
-      <button type="submit" class="os-btn w-100">Register Now</button>
-      <div class="or-divide"><span>or</span></div>
-      <nuxt-link href="/login" class="os-btn os-btn-black w-100">login Now</nuxt-link>
+    <div class="mt-10"></div>
+    <button type="submit" class="os-btn w-100">Register Now</button>
+    <div class="or-divide"><span>or</span></div>
+    <nuxt-link href="/login" class="os-btn os-btn-black w-100">login Now</nuxt-link>
   </Form>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import { Field, Form, ErrorMessage } from "vee-validate";
-import * as yup from "yup";
+<script setup lang="ts">
+import { Field, Form, ErrorMessage } from 'vee-validate';
+import * as yup from 'yup';
 
-export default defineComponent({
-  components: { Field, Form, ErrorMessage },
-  setup() {
-    const schema = yup.object({
-      name: yup.string().required().label("Name"),
-      email: yup.string().required().email().label("Email"),
-      password: yup.string().required().min(6).label("Password"),
-    });
-
-    function onSubmit(values: object,{resetForm}: {resetForm: () => void}) {
-      alert(JSON.stringify(values, null, 2));
-      resetForm()
-    }
-    return { schema, onSubmit };
-  },
+const schema = yup.object({
+  name: yup.string().required('請輸入姓名').label('Name'),
+  email: yup.string().required('請輸入信箱').email().label('Email'),
+  password: yup.string().required('請輸入密碼').min(6).label('Password'),
 });
-</script>
 
+function onSubmit(values: object, { resetForm }: { resetForm: () => void }) {
+  console.log('🚀 ~ onSubmit ~ values:', values);
+  alert(JSON.stringify(values, null, 2));
+  resetForm();
+}
+</script>
--- pages/register.vue
+++ pages/register.vue
@@ -1,26 +1,27 @@
 <template>
   <layout :transparent="true">
-    <breadcrumb-area title="Register" subtitle="Register"/>
-    <register-area/>
+    <breadcrumb-area title="Register" subtitle="Register" />
+    <section class="login-area pt-100 pb-100">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 offset-lg-2">
+            <div class="basic-login">
+              <h3 class="text-center mb-60">註冊</h3>
+              <register-form />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </layout>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import Layout from "../layout/Layout.vue";
-import BreadcrumbArea from "../components/common/breadcrumb/BreadcrumbArea.vue";
-import RegisterArea from "../components/login-register/RegisterArea.vue";
+<script setup lang="ts">
+import Layout from '../layout/Layout.vue';
+import BreadcrumbArea from '../components/common/breadcrumb/BreadcrumbArea.vue';
+import RegisterForm from '../components/forms/RegisterForm.vue';
 
-export default defineComponent({
-  components: {
-    Layout,
-    BreadcrumbArea,
-    RegisterArea,
-  },
-  setup() {
-    useHead({
-      title: "Register - outStock",
-    });
-  },
+useHead({
+  title: '註冊',
 });
 </script>
